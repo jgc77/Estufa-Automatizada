3410be11cc339fe18959db18206bdca9d8e780b2
João Gustavo Cavalcanti Beltrão da Silva <joaogustavocavalcante@gmail.com>
2024-10-22 11:50:31 -0300

    upload biblioteca menu

---
+++ MenuLCD/MenuLCD.h
@@ -0,0 +1,33 @@
+#ifndef MenuLCD_h
+#define MenuLCD_h
+
+#include <Arduino.h>
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+
+class MenuLCD {
+  public:
+    MenuLCD(uint8_t lcd_addr, uint8_t lcd_cols, uint8_t lcd_rows, uint8_t btn1_pin, uint8_t btn2_pin);
+    void iniciar();
+    void atualizar();
+    void atualizarLuminosidade();
+    void setTemperatura(float temp);
+    void setUmidade(float umid);
+    void setLuminosidade(float lumi);
+
+  private:
+    LiquidCrystal_I2C lcd;
+    uint8_t botao_1;
+    uint8_t botao_2;
+    int menu_atual;
+    bool dentro_submenu;
+    int estado_anterior_botao_1;
+    int estado_anterior_botao_2;
+    float temperatura;
+    float umidade;
+    float luminosidade;
+    void mostrarMenu();
+    void mostrarSubmenu();
+};
+
+#endif
